b2cfdfb905ba15f79e42c333a12296459c1bbbaf
Jose Montana <jamontanac@unal.edu.co>
2025-06-17 15:49:57 -0500

    formatting

--- src/ecom_scrapper/utils.py
+++ src/ecom_scrapper/utils.py
@@ -1,11 +1,74 @@
+import json
 import logging
+import os
 import pathlib
+import subprocess
+from typing import List
 
 import colorlog
+import requests
 import yaml
 
 
+def get_updated_proxy_list(country_codes: List[str]) -> None:
+    """Retrieves the updated proxy_list.
+
+    this function is based on the repo of
+    https://github.com/vakhov/fresh-proxy-list/tree/master
+
+    Args:
+        contry_code: str, The code of certain country
+
+    """
+    url = os.getenv("PROXY_URL", "https://vakhov.github.io/fresh-proxy-list/proxylist.json")
+
+    proxy_path = pathlib.Path(get_project_root()).joinpath("data", "proxies", "proxylist.json")
+    proxy_path.parent.mkdir(parents=True, exist_ok=True)
+
+    # make request to get the updated file
+    response = requests.get(url, timeout=3)
+    response.raise_for_status()
+
+    # create the jq filter
+    countries_json = json.dumps(country_codes)
+    jq_filter = """
+            [.[] | select(
+                (.country_code as $cc | $countries | index($cc)) 
+                and 
+                (.socks4 == "0") 
+                and 
+                (.socks5 == "0")
+            )]
+            """.strip()
+
+    try:
+        result = subprocess.run(
+            ["jq", "--argjson", "countries", countries_json, jq_filter],
+            input=response.text,
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        with open(proxy_path, "w", encoding="utf-8") as f:
+            f.write(result.stdout)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(f"jq error: {e.stderr}") from e
+
+    except FileNotFoundError:
+        raise RuntimeError("jq command not found - install jq first") from None
+
+
 def get_logger(logger_name):
+    """Create and return logger.
+
+    logger is created to log into the console with log level Info.
+    the string format will difer if the logger is being created from a script or from a module.
+
+    Args:
+        logger_name: str, the name of the logger
+    Returns:
+        logging.Logger: the logger
+    """
     logger = logging.getLogger(logger_name)
     if logger.hasHandlers():
         logger.handlers = []
@@ -37,12 +100,13 @@ def get_project_root():
         str: The absolute path to the project root directory.
     """
     current_path = pathlib.Path(__file__).resolve()
+
     potential_root = current_path.parent  # Adjust the number of parents as needed
     for _ in range(4):  # check up to 4 levels up
         if (potential_root / "pyproject.toml").exists():
             return potential_root.as_posix()
         potential_root = potential_root.parent
-    return current_path.parent.as_posix()
+    return current_path.as_posix()
 
 
 def read_yaml_file(file):
